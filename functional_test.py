d21c33a785e328903df8f3b71c10c0c328e5f56e
Guilherme Ribeiro de Souza <guilherme.souza@qitech.com.br>
2021-01-31 20:24:26 -0300

    redirect after POST, and show all items in template

--- functional_test.py
+++ functional_test.py
@@ -50,7 +50,7 @@ class NewVisitorTest(unittest.TestCase):
         #ainda continua havendo uma caixa de texto convidando-a a acrrescentar outros
         #item. Ela insere "Use peackock feathers to make a fly" (usar penas de pavãos para dazer um fly - Edith é bem metódica)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feather to ake a fly')
+        inputbox.send_keys('Use peacock feather to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
