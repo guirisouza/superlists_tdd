d29ca988e539b70cabf74095de9e4b7db5c66171
Guilherme Ribeiro de Souza <guilherme.souza@qitech.com.br>
2021-01-31 18:54:58 -0300

    created ItemModel

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import ItemModel
 from django.urls import resolve
 from .views import home_page
 from django.http import HttpRequest
@@ -15,3 +16,20 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_saving_and_retrieving_items(self):
+        first_item = ItemModel()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = ItemModel()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = ItemModel.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
